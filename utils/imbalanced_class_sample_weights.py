14a1fedfffa2c10713b4cba80b44e2c22476af61
alex4321 <gaussmake@gmail.com>
2017-09-11 07:38:02 +0300

    Fixed sample_weight calculation for imbalanced classes

--- utils/imbalanced_class_sample_weights.py
+++ utils/imbalanced_class_sample_weights.py
@@ -5,7 +5,7 @@ def imbalanced_class_sample_weights(y_true):
     labels = np.unique(y_true).tolist()
     labels.sort()
     weights = np.zeros(y_true.shape)
-    for label in enumerate(labels):
+    for label in labels:
         labeled_index = y_true == label
         labeled_count = labeled_index.sum()
         label_weight = len(y_true) / labeled_count
